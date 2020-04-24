8bc0429ecf430c92cb8a44d819848fff52330a9a
Lolinfamous <62343973+Lolinfamous@users.noreply.github.com>
2020-04-24 10:44:31 +0300

    ok

---
+++ Зад2/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace Зад2
+{
+    class Program
+    {   
+        class Rational
+        {
+            private int m, n;
+
+            private static int nod(int a, int b)
+            {
+                a = Math.Abs(a); b = Math.Abs(b);
+                while (a != 0 && b != 0)
+                {
+                    if (a > b) a %= b;
+                    else b %= a;
+                }
+                return a + b;
+            }
+            public Rational(int a, int b)
+            {
+                if (b == 0)
+                {
+                    this.m = 0;
+                    this.n = 1;
+                }
+                else
+                {
+                    if (a < 0 && b < 0) { a = Math.Abs(a); b = Math.Abs(b); }
+                    if (b < 0) { b *= -1; a *= -1; }
+                    int c = nod(a, b);
+                    this.m = a/c;
+                    this.n = b/c;
+
+                }
+            }
+            public override string ToString() {
+                return m + "/" + n;
+            }
+
+            public void PrintRational()
+            {
+                Console.WriteLine("{0}/{1}", m, n);
+            }
+            public Rational Plus(Rational a)
+            {
+                int b, c;
+                b = m * a.n + n * a.m;
+                c = n * a.n;
+                int d = nod(b, c);
+                return (new Rational(b / d, c / d));
+            }
+            public static Rational operator +(Rational a, Rational b)
+            {
+                return (a.Plus(b));
+            }
+
+            public Rational Minus(Rational a)
+            {
+                int b, c;
+                b = m * a.n - n * a.m;
+                c = n * a.n;
+                int d = nod(b, c);
+                return (new Rational(b/d, c/d));
+            }
+            public static Rational operator -(Rational a, Rational b)
+            {
+                return (a.Minus(b));
+            }
+
+            public Rational Mult(Rational a)
+            {
+                int b, c;
+                b = m * a.m;
+                c = n * a.n;
+                int d = nod(b, c);
+                return (new Rational(b / d, c / d));
+            }
+            public static Rational operator *(Rational a, Rational b)
+            {
+                return (a.Mult(b));
+            }
+
+            public Rational Divide(Rational a)
+            {
+                int b, c;
+                b = m * a.n;
+                c = n * a.m;
+                int d = nod(b, c);
+                return (new Rational(b / d, c / d));
+            }
+            public static Rational operator /(Rational a, Rational b)
+            {
+                return (a.Divide(b));
+            }
+            private Rational(int a, int b, string t)
+            {
+                m = a; n = b;
+            }
+            public static readonly Rational Zero, One;
+            static Rational()
+            {
+                Zero = new Rational(0, 1, "private");
+                One = new Rational(1, 1, "private");
+            }
+            public static bool operator ==(Rational a, Rational b)
+            {
+                return ((a.m == b.m) && (a.n == b.n));
+            }
+            public static bool operator !=(Rational a, Rational b)
+            {
+                return ((a.m != b.m) || (a.n != b.n));
+            }
+            public static bool operator >(Rational a, Rational b)
+            {
+                return ((a.m * b.n) > (a.n * b.m));
+            }
+            public static bool operator <(Rational a, Rational b)
+            {
+                return ((a.m * b.n) < (a.n * b.m));
+            }
+            public static bool operator <(Rational a, double b)
+            {
+                return ((double)a.m / a.n < b);
+            }
+            public static bool operator >(Rational a, double b)
+            {
+                return ((double)a.m / a.n > b);
+            }
+            public static void TestRational()
+            {
+                Rational d1 = new Rational(1, 1);
+                Rational d2 = new Rational(7, 3);
+                Rational c5 = new Rational(5, 10);
+                Rational c6 = new Rational(-2, 8);
+                double a = 0.6;
+                double b = -0.6;
+                if (d1 == One) Console.WriteLine("Равно");
+                if (d2 != Zero) Console.WriteLine("Неравно");
+                if (c6 < c5) Console.WriteLine("Меньше");
+                if (c5 > Zero) Console.WriteLine("Больше");
+                if (c5 < a) Console.WriteLine("Меньше");
+                if (c6 > b) Console.WriteLine("Больше");
+            }
+        }
+        static void Main()
+        {
+            Rational c = new Rational(-2, -5);
+            Rational c1 = new Rational(0, 10);
+            Rational c2 = new Rational(5, -5);
+            Rational c3 = new Rational(2, 3);
+            Rational c4 = new Rational(4, 8);
+            Console.WriteLine(c.ToString());
+            c1.PrintRational();
+            c2.PrintRational();
+            c3.PrintRational();
+            c1 = c + c4;
+            c1.PrintRational();
+            c1 = c - c4;
+            c1.PrintRational();
+            c1 = c * c4;
+            c1.PrintRational();
+            c1 = c / c4;
+            c1.PrintRational();
+
+            Rational.TestRational();
+        }
+    }
+}
---
+++ Лаб7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Лаб7
+{
+    class Program
+    {
+        class Example
+        {
+            private string fam, status;
+            private int salary, age;
+            public string Fam
+            {
+                set { if (fam == "") fam = value; }
+                get { return (fam); }
+            }
+
+            public int Age
+            {
+                set { age = value; }
+                get { return (age); }
+            }
+
+            public string Status
+            {
+                get { return (status); }
+            }
+
+            public int Salary
+            {
+                set { salary = value; }
+            }   
+
+            private string helth;
+            public Example()
+            {
+                this.fam = "";
+                this.helth = "Алкоголик";
+                this.status = "Холост";
+            }
+            public int get_salary()
+            {
+                return this.salary;
+            }
+
+            public string get_helth()
+            {
+                return this.helth;
+            }
+
+        }
+        static void Main()
+        {
+            Example c = new Example();
+            c.Fam = "Кукса";
+            c.Fam = "hggj";
+            c.Salary = 49950;
+            c.Age = 22;
+            Console.WriteLine("Фамилия = {0}, статус = {1}, зарплата = {2}, возраст = {3}, здоровье = {4}", c.Fam, c.Status, c.get_salary(), c.Age, c.get_helth());
+        }
+    }
+}
